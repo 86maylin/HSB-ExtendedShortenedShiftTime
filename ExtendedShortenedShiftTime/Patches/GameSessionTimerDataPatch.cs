6b8d3497a00b036d6076a23c0100523360c8240a
86maylin <86maylin@gmail.com>
2024-04-16 15:33:55 +0800

    Initial commit + gitignore

---
+++ ExtendedShortenedShiftTime/Patches/GameSessionTimerDataPatch.cs
@@ -0,0 +1,49 @@
+﻿using BBI.Unity.Game;
+using HarmonyLib;
+
+namespace ExtendShortenShiftTime.Patches
+{
+    [HarmonyPatch(typeof(GameSessionTimerData))]
+    internal class GameSessionTimerDataPatch
+    {
+
+        [HarmonyPatch(nameof(GameSessionTimerData.FromLevelAndDifficultyData))]
+        [HarmonyPostfix]
+        static void FromLevelAndDifficultyData_Postfix(LevelAsset.LevelData levelData, DifficultyModeAsset difficultyMode, ref GameSessionTimerData __result)
+        {
+            ExtendShortenShiftTime.LoggerInstance.LogInfo("FromLevelAndDifficultyData_Postfix");
+            bool enabled = false;
+            bool active = false;
+            float completionTimeInSeconds = levelData.CompletionTimeInSeconds;
+            bool flag = (difficultyMode != null) ? difficultyMode.TimerCountsUp : levelData.TimerCountsUp;
+            if (flag)
+            {
+                enabled = true;
+                active = true;
+            }
+            else if (completionTimeInSeconds > 0f)
+            {
+                enabled = true;
+                active = true;
+            }
+            if (levelData.SessionType == GameSession.SessionType.Career && levelData.CompletionTimeInSeconds != 0)
+            {
+                float value;
+                if (ExtendShortenShiftTime.Config_ShiftTimeInSeconds.Value)
+                {
+                    value = ExtendShortenShiftTime.Config_shiftTime.Value;
+                }
+                else
+                {
+                    value = ExtendShortenShiftTime.Config_shiftTime.Value * 60;
+                }
+                ExtendShortenShiftTime.LoggerInstance.LogInfo("Set shift time to: " + value);
+                __result = new GameSessionTimerData(flag, value, value, enabled, active);
+            }
+            else
+            {
+                __result = new GameSessionTimerData(flag, flag ? 0f : completionTimeInSeconds, flag ? 36000f : completionTimeInSeconds, enabled, active);
+            }
+        }
+    }
+}
---
+++ ExtendedShortenedShiftTime/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using ExtendShortenShiftTime.Patches;
+using HarmonyLib;
+
+namespace ExtendShortenShiftTime
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ExtendShortenShiftTime : BaseUnityPlugin
+    {
+        private const string modGUID = "86maylin.extend_shorten_shift_time";
+        private const string modName = "Extend/Shorten Shift Time";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static ConfigEntry<bool> Config_enabled { get; private set; }
+        public static ConfigEntry<int> Config_shiftTime { get; private set; }
+        public static ConfigEntry<bool> Config_ShiftTimeInSeconds { get; private set; }
+        public static ManualLogSource LoggerInstance;
+
+        void Awake()
+        {
+            LoggerInstance = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            SetConfigs();
+            if (Config_enabled.Value)
+            {
+                harmony.PatchAll(typeof(ExtendShortenShiftTime));
+                harmony.PatchAll(typeof(GameSessionTimerDataPatch));
+                LoggerInstance.LogInfo("ExtendShortenShiftTime patched.");
+            }
+            else
+            {
+                LoggerInstance.LogInfo("ExtendShortenShiftTime disabled.");
+            }
+        }
+
+        private void SetConfigs()
+        {
+            Config_enabled = Config.Bind("Config", "Enabled", true, "Whether to enable the plugin or not.");
+            Config_shiftTime = Config.Bind("Config", "ShiftTime", 20, "Shift time in minutes(if ShiftTimeInSeconds is true then it will be in seconds).");
+            Config_ShiftTimeInSeconds = Config.Bind("Config", "ShiftTimeInSeconds", false, "Whether to specify ShiftTime in seconds or not(to allow finer tunning).");
+        }
+    }
+}
